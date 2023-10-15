dc832015ac44aa003f6debdbabbe7c5ade942f24
Pedro Paulo da Silva Monteiro <pedropaulopyter6@gmail.com>
2023-10-15 19:38:44 -0300

    menu done

--- src/components/navgresponsive.tsx
+++ src/components/navgresponsive.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from 'react-scroll';
 import '../styles/navgresponsive.module.css'
 import style from '../styles/navgresponsive.module.css';
 import Img from '../imgs/iconmenu.png';
 
 function Navgres(){
+    const [isHomeLinkHovered, setIsHomeLinkHovered] = useState(true);
+    const [isSkillsLinkHovered, setIsSkillsLinkHovered] = useState(false);
+    const [isContactLinkHovered, setIsContactLinkHovered] = useState(false);
+    const [isProjectLinkHovered, setIsProjectLinkHovered] = useState(false);
+   
  const [showmenu,setShowmenu] = useState(false)
  const funcShowmenu =()=>{
     setShowmenu(!showmenu);
@@ -17,14 +23,34 @@ function Navgres(){
             
             <span className={style.spanmenu}>
             <img  onClick={funcShowmenu} className={style.menu} src={Img} 
-            style={{width:30,height:30,right:0,backgroundColor:showmenu ? 'red': 'blue'}}>
+            style={{width:30,height:30,right:0,backgroundColor:showmenu ? '': ''}}>
             </img>
             <div style={{display:showmenu ? 'flex': 'none'}} className={style.menulist}>
             <ul>
-                <li>Home</li>
-                <li>Skills</li>
-                <li>Projetos</li>
-                <li>Contato</li>
+                <li><Link
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="/">Home</Link></li>
+                <li><Link
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="skills">Skills</Link></li>
+                <li><Link
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="projets">Projetos</Link></li>
+                <li><Link
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={500}
+              to="contact">Contato</Link></li>
             </ul>
             </div>
             </span>
